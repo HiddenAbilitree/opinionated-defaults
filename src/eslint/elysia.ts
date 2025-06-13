a8fa321a4a1033d7cd5501febf1e53e641c6c8a3
Eric Zhang <me@ericzhang.dev>
2025-06-13 03:28:46 -0400

    fix: elysia configs

--- src/eslint/elysia.ts
+++ src/eslint/elysia.ts
@@ -2,6 +2,6 @@ import baseConfig from './base.ts';
 import tsConfig from './typescript.ts';
 import functionalConfig from './functional.ts';
 
-const elysiaConfig = [baseConfig, tsConfig, functionalConfig];
+const elysiaConfig = [...baseConfig, ...tsConfig, ...functionalConfig];
 
 export default elysiaConfig;
