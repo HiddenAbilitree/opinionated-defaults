bdfbe323dba8c313dc05a3fdd03d872c4db17f55
Eric Zhang <me@ericzhang.dev>
2025-06-13 03:28:46 -0400

    fix: elysia configs

--- src/eslint/react.ts
+++ src/eslint/react.ts
@@ -2,7 +2,7 @@ import pluginReactHooks from 'eslint-plugin-react-hooks';
 import pluginReact from 'eslint-plugin-react';
 
 const reactConfig = [
-  pluginReact.configs.flat.all,
+  pluginReact.configs.flat['all'],
   pluginReactHooks.configs['recommended-latest'],
 ];
 
