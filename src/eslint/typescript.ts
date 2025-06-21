89f5b05a27e8f4cef2131cd1f77d89f1658c9fe2
Eric Zhang <me@ericzhang.dev>
2025-06-20 21:11:26 -0400

    fix: build

--- src/eslint/typescript.ts
+++ src/eslint/typescript.ts
@@ -1,7 +1,8 @@
 import parser from '@typescript-eslint/parser';
 import tseslint from 'typescript-eslint';
+import { type Config } from 'typescript-eslint';
 
-const typescriptConfig = [
+const typescriptConfig: Config = [
   ...tseslint.configs.recommended,
   {
     languageOptions: {
