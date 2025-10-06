9db3579a62624f0bbde433d94027d954c41b81a8
Eric Zhang <me@ericzhang.dev>
2025-10-06 11:15:27 -0400

    fix: remove "React version not specified" message

--- src/eslint/react.ts
+++ src/eslint/react.ts
@@ -2,11 +2,24 @@ import type { ConfigArray } from 'typescript-eslint';
 
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
 
-const reactConfig: ConfigArray = [
+const reactConfig: ConfigArray = defineConfig([
   pluginReact.configs.flat[`recommended`]!,
   pluginReact.configs.flat[`jsx-runtime`]!,
-  pluginReactHooks.configs[`recommended-latest`],
-];
+  {
+    extends: [`react-hooks/recommended`],
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: `detect`,
+      },
+    },
+  },
+]);
 
 export default reactConfig;
